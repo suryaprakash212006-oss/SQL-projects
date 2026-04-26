dbc7e96e326c4d50f78228ca0e1356971fa199bc
suryaprakash212006-oss <suryaprakash212006@gmail.com>
2026-04-26 13:11:47 +0530

    Add files via upload

--- Assignment_M.Surya prakash.sql
+++ Assignment_M.Surya prakash.sql
@@ -1,54 +1,55 @@
 USE company_db;
+-- 1.Update the Salary of Employees in the 'Sales' Department by 10%
+UPDATE employees e
+JOIN departments d
+ON e.department_id=d.department_id
+SET e.salary=e.salary*1.10
+WHERE d.department_name='Sales';
 
--- 1.Transaction To Transfer Salary
-START TRANSACTION;
-UPDATE employees
-SET salary=salary-1000
-WHERE employee_id=101;
-UPDATE employees
-SET salary=salary+1000
-WHERE employe_id=102;
-COMMIT;
-SELECT employee_id,first_name,salary
-FROM employees
-WHERE employee_id IN (101,102);
+SELECT*FROM employees;
 
--- 2.Departments with Employees
-SELECT d.*
-FROM departments d 
-WHERE EXISTS(
-SELECT 1
+-- 2.Delete Employees Working in a Department Named 'Obsolete'
+DELETE e 
 FROM employees e 
-WHERE e.department_id=d.department_id);
+JOIN departments d 
+ON e.department_id=d.department_id
+WHERE d.department_name='Obsolete';
 
--- 3.Recursive CTE for employee_Manager Hierarchy
-WITH RECURSIVE emp_hierarchy AS (
-SELECT employee_id,first_name,last_name,manager_id
+SELECT*FROM employees;
+
+-- 3.Create a View Showing High-Earning Employees(Salary>80,000)
+CREATE VIEW high_earning_employees AS
+SELECT *
 FROM employees
-WHERE manager_id IS NULL
+WHERE salary>80000;
 
-UNION ALL 
+SELECT * FROM employees;
 
-SELECT e.employee_id,e.first_name,e.last_name,e.manager_id
-FROM employees e
-JOIN emp_hierarchy eh 
-ON e.manager_id=eh.employee_id
-)
-SELECT*FROM emp_hierarchy;
+-- 4.Add a Check Constraints to Ensure Salary>0
+ALTER TABLE employees
+ADD CONSTRAINT chk_salary
+CHECK(salary>0);
 
--- 4.LEAD and LAG for salaries
-SELECT 
-employee_id,
-first_name,
-last_name,
-salary,
-LAG(salary)OVER(ORDER BY salary) AS previous_salary, 
-LEAD(salary)OVER(ORDER BY salary) AS next_salary
-FROM employees;
+INSERT INTO employees
+(employee_id,first_name,last_name,department_id,salary,hire_data,job_title)
+VALUES(999,'test','user',101,-5000,'2024-01-01','tester');
 
--- 5.Salary Toatals with ROLLUP
-SELECT
-department_id,
-SUM(salary)AS total_salary
-FROM employees
-GROUP BY department_id WITH ROLLUP;
+-- 5.Create an Index on Employee Last Names for Faster Searching
+CREATE INDEX idx_last_name
+ON employees(last_name);
+
+SHOW INDEX FROM employees;
+
+-- 6.Create a Stored Procedure to Retrieve Employees of a Given Department
+DELIMITER //
+CREATE PROCEDURE get_employees_by_department(IN dept_name VARCHAR(100))
+BEGIN
+SELECT e.*
+FROM employees e  
+JOIN departments d 
+ON e.department_id=d.department_id
+WHERE d.department_name=dept_name;
+END //
+DELIMITER ;
+ 
+CALL get_employees_by_department('Sales');
